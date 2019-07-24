5181115a012d5ce59e7b7ceb8d90bb926fc0f62b
Flávio Pangrácio <flaviohugo.14@gmail.com>
2019-07-24 18:32:52 -0300

    feat: add state

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const readline = require('readline-sync')
 const robots = {
-    //userInput: require('./robots/user-input.js')
-    text: require('./robots/text.js')
+    input: require('./robots/input.js'),
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js')
 }
 async function start(){
-    const content = {
-        maximumSentences: 7
-    }
-
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-    
-    //robots.userInput(content)
-    await robots.text(content)
-
-    function askAndReturnSearchTerm(){
-        return readline.question('Type a Wikipedia search term: ')
-    }
-
-    function askAndReturnPrefix(){
-        const prefixes = ['Who is', 'What is', 'The History of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one options: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-        
-        return selectedPrefixText
-    }
-
-    console.log(JSON.stringify(content, null, 4))
+    robots.input()
+    await robots.text()
+    const content = robots.state.load()
+    console.dir(content, { depth: null } )
 }
 
 start()
